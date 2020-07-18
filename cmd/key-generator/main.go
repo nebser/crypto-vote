9165defa407acbe7a1c9b9f5705aa0b105eb1e98
nebser <nebojsa.serafimovski@gmail.com>
2020-07-19 01:36:10 +0200

    Modified key generator to support flags
    Added flags to key generator for following things:
    	1. Directory location for alfa node's keypair
    	2. Directory location for clients keypairs
    	3. Number of client keypairs to generate

--- cmd/key-generator/main.go
+++ cmd/key-generator/main.go
@@ -1,40 +1,38 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
-	"os"
-	"strconv"
 
 	"github.com/nebser/crypto-vote/internal/pkg/wallet"
 )
 
 func main() {
-	wallets := wallet.Wallets{}
-	num := 10
-	if len(os.Args) > 1 {
-		parsed, err := strconv.Atoi(os.Args[1])
-		if err != nil {
-			log.Fatal("Argument must be an integer")
-		}
-		num = parsed
-	}
-	for i := 0; i < num; i++ {
+	alfaKeyDir := flag.String("alfa", "alfa", "Directory where to create key pairs for alfa node")
+	clientKeysDir := flag.String("clients", "clients", "Directory where to create client key pairs")
+	num := flag.Int("clientsNumber", 50, "Number of client key pairs to generate")
+	flag.Parse()
+
+	clientWallets := wallet.Wallets{}
+	for i := 0; i < *num; i++ {
 		w, err := wallet.New()
 		if err != nil {
 			log.Fatal(err)
 		}
-		wallets = append(wallets, *w)
+		clientWallets = append(clientWallets, *w)
 	}
-	for i, w := range wallets {
-		if err := w.Export(fmt.Sprintf("wallets/w%d", i)); err != nil {
+	for i, w := range clientWallets {
+		if err := w.Export(fmt.Sprintf("%s/c%d", *clientKeysDir, i)); err != nil {
 			log.Fatal(err)
 		}
 	}
 
-	w, err := wallet.Import("wallets/w0_pub.pem", "wallets/w0.pem")
+	alfaWallet, err := wallet.New()
 	if err != nil {
+		log.Fatalf("Failed to create wallet for alfa node. Error %s", err)
+	}
+	if err := alfaWallet.Export(fmt.Sprintf("%s/key", *alfaKeyDir)); err != nil {
 		log.Fatal(err)
 	}
-	log.Println(w.Address)
 }
